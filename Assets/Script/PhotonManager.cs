ea05bd1b1a7d7eab854ac9b281376422d5046742
BaeGWoo <gwbae4211@naver.com>
2022-07-14 21:48:17 +0900

    Photon Room

--- Assets/Script/PhotonManager.cs
+++ Assets/Script/PhotonManager.cs
@@ -33,4 +33,31 @@ public class PhotonManager : MonoBehaviourPunCallbacks
                 break;
         }
     }
+
+
+    //포톤 서버에 접속 후 호출되는 콜백 함수
+    //로비에 접속했는지 확인하는 함수
+    public override void OnConnectedToMaster()
+    {
+        switch(Data.count)
+        {
+            case 0:
+                PhotonNetwork.JoinLobby(new TypedLobby("Lobby 1",LobbyType.Default));
+                break;
+            case 1:
+                PhotonNetwork.JoinLobby(new TypedLobby("Lobby 2", LobbyType.Default));
+                break;
+            case 2:
+                PhotonNetwork.JoinLobby(new TypedLobby("Lobby 3", LobbyType.Default));
+                break;
+        }
+    }
+
+    //로비에 접속 후 호출되는 콜백 함수
+    public override void OnJoinedLobby()
+    {
+        //PhotonNetwork.LoadLevel을 사용하는 이유는 씬 동기화를 맞추기 위해 사용
+        //일반 LoadLevel은 씬 동기화가 되지 않습니다.        
+        PhotonNetwork.LoadLevel("Photon Room");
+    }
 }
