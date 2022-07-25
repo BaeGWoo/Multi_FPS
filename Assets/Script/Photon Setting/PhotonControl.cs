70d517bd4a44e9db83487c137449faac9f1f2e02
BaeGWoo <gwbae4211@naver.com>
2022-07-25 21:46:14 +0900

    Damage System

--- Assets/Script/Photon Setting/PhotonControl.cs
+++ Assets/Script/Photon Setting/PhotonControl.cs
@@ -5,15 +5,22 @@ using UnityEngine.UI;
 using Photon.Pun;
 
 
-public class PhotonControl : MonoBehaviourPun
+public class PhotonControl : MonoBehaviourPun , IPunObservable
 {
     public float speed = 5.0f;
     public float angleSpeed;
 
+    public LayerMask layer;
     public Camera cam;
 
+    public int health=100;
+    RaycastHit hit;
+
+    public Texture2D cursorImage;
     private void Start()
     {
+
+        Cursor.SetCursor(cursorImage, Vector2.zero, CursorMode.ForceSoftware);
         //현재 플레이어가 나 자신이라면
         if(photonView.IsMine)
         {
@@ -24,6 +31,21 @@ public class PhotonControl : MonoBehaviourPun
         {
             cam.enabled = false;
             GetComponentInChildren<AudioListener>().enabled = false;
+        }
+    }
+
+    // IPunObservable 인터페이스를 구현해야한다.
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            // We own this player: send the others our data
+            stream.SendNext(health);
+        }
+        else
+        {
+            // Network player, receive data
+            this.health = (int)stream.ReceiveNext();
         }
     }
 
@@ -42,6 +64,34 @@ public class PhotonControl : MonoBehaviourPun
             0
             );
 
+
         
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //Damage();
+
+        if(Input.GetMouseButton(0))
+        {
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity,layer))
+            {
+                photonView.RPC("Damage", RpcTarget.All);
+            }
+        }
+       
+
+       
+    }
+
+    [PunRPC]
+    public void Damage()
+    {
+        if (health <= 0)
+        {
+            PhotonNetwork.Destroy(gameObject);
+        }
+
+
+        hit.transform.GetComponent<PhotonControl>().health -= 10;
     }
 }
